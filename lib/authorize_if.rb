552f979f49102f15872a58db52fb354645914fac
Vladimir Rybas <vladimirrybas@gmail.com>
2016-08-26 11:51:33 +0700

    Cleanup "No authorization rule defined" error msg generation

--- lib/authorize_if.rb
+++ lib/authorize_if.rb
@@ -138,12 +138,9 @@ module AuthorizeIf
     rule_method_name = "authorize_#{action_name}?"
 
     unless self.respond_to?(rule_method_name, true)
-      msg = [
-        "No authorization rule defined for action",
-        "#{controller_name}##{action_name}.",
-        "Please define method ##{rule_method_name} for",
-        self.class.name
-      ].join(' ')
+      msg = "No authorization rule defined for action"\
+            " #{controller_name}##{action_name}."\
+            " Please define method ##{rule_method_name} for #{self.class.name}"
 
       raise(MissingAuthorizationRuleError, msg)
     end
@@ -157,4 +154,3 @@ if defined?(ActiveSupport)
     include AuthorizeIf
   end
 end
-
